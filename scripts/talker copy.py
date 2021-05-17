60bad023d692d8572ccb9537df44ae758a8ce2d0
dmigranov <d.migranov@g.nsu.ru>
2021-05-17 15:42:35 +0700

    talker

--- scripts/talker copy.py
+++ scripts/talker copy.py
@@ -32,133 +32,115 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 # Revision $Id$
- 
+
 ## Simple talker demo that published std_msgs/Strings messages
 ## to the 'chatter' topic
- 
+
 import rospy
-import numpy as np
-from std_msgs.msg import Float64
+from std_msgs.msg import String
 import math
 import matplotlib.pyplot as plt
- 
+
+from math import sqrt
+
 x = []
 y = []
- 
-dx_arr = []
-dy_arr = []
-fi_ref_arr = []
  
 def init():
     x_k = 0
     y_k = 0
     fi_k = 0
     return x_k, y_k, fi_k
- 
+
 def step(x_k, y_k, fi_k, dt, v, omega):
-    x_k = x_k + dt*v*math.sin(fi_k)
-    y_k = y_k + dt*v*math.cos(fi_k)
+    x_k = x_k + dt * v * math.sin(fi_k)
+    y_k = y_k + dt * v * math.cos(fi_k)
     fi_k = fi_k + omega * dt
     return x_k, y_k, fi_k
- 
-def go_forward_one(velocity, x_k, y_k, fi_k):
-    x_k, y_k, fi_k = step(x_k, y_k, fi_k, dt, velocity, 0)
-    return x_k, y_k, fi_k
-    
- 
-def go_to_point(x_k, y_k, fi_k, x_ref, y_ref, v, dt):
+
+def go_to_point(x_k, y_k, fi_k, x_ref, y_ref, dt):
     global x,y
-    global dx_arr, dy_arr, fi_ref_arr
- 
-    coeff = 1
+    coeff1 = 1
+    coeff2 = 0.1
     
     dx = x_ref - x_k
     dy = y_ref - y_k
-    fi_ref = math.atan2(dx, dy)
-    omega = -(fi_k - fi_ref) * coeff
-    
- 
-    x_k_o = x_k
-    y_k_o = y_k
-    
+
+#    fi_ref = math.atan2(dy, dx)   #можно вычислять вне цикла, для оптимизации?
+    fi_ref = math.atan(dy/dx)
+
+    if dy < 0:
+    	fi_ref = math.pi + fi_ref
+
+    omega = -(fi_k - fi_ref) * coeff1
+
+
+    v = sqrt(dx ** 2 + dy ** 2) * coeff2
     x_k, y_k, fi_k = step(x_k, y_k, fi_k, dt, v, omega)
+
+    x.append(x_k)
+    y.append(y_k)
+
+    return x_k, y_k, fi_k
+
+def go_to_point_opti(x_k, y_k, fi_k, x_ref, y_ref, fi_ref, v, dt):
+    #сначала поворот до нужного, потом движение
+    global x,y
+    coeff = 1
     
-    dxdt = (x_k - x_k_o)/dt
-    dydt = (y_k - y_k_o)/dt
-    
-    dx_arr.append(dxdt)
-    dy_arr.append(dydt)
-    fi_ref_arr.append(math.atan2(dxdt, dydt))
-    
+    #if close fi_k to fi_ref
+        #move
+    #else
+        #rotate
+
+    omega = -(fi_k - fi_ref) * coeff
+
+    x_k, y_k, fi_k = step(x_k, y_k, fi_k, dt, v, omega)
+
     x.append(x_k)
     y.append(y_k) 
-    return x_k, y_k, fi_k, omega
-    
- 
-    
- 
+    return x_k, y_k, fi_k
+
+
+
+
 def talker():
-    pub = rospy.Publisher('omega_chatter', Float64, queue_size=10)
+    pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(100) # 10hz
+    rate = rospy.Rate(100) # 100hz - 0.01 s
  
     x_k, y_k, fi_k = init()
- 
-    v = 1
-    omega = math.pi/2
- 
-    
-    x_ref = 3
-    y_ref = 3
-    
-   
-    eps = 5e-2
- 
+
+    v = 1.
+
+    x_ref = -10.
+    y_ref = 10.
+
+    dx = x_ref - x_k
+    dy = y_ref - y_k
+    fi_ref = math.atan2(dx, dy) 
+
     start_time = rospy.get_time()
- 
+
     while not rospy.is_shutdown():
         end_time = rospy.get_time()
         dt = end_time - start_time
         start_time = end_time
-        
- 
-        #x_k, y_k, fi_k = step(x_k, y_k, fi_k, dt, v, omega)
-        
-        x_k, y_k, fi_k, omega = go_to_point(x_k, y_k, fi_k, x_ref, y_ref, v, dt)
-        if abs(x_k - x_ref) < eps and abs(y_k - y_ref) < eps:
-            v = 0
-        
-        x_k_n  = x_k + np.random.normal(0, 0.003)
-        y_k_n  = y_k + np.random.normal(0, 0.003)
-        
- 
-        
-        
-        #x_k = x_k_n
-        #y_k = y_k_n
-        fi_k = fi_k + np.random.normal(0, 1.5*math.pi/180.0)
-        
-        #hello_str = "x_k = {} y_k = {} fi_k = {} dt = {} ".format(x_k, y_k, fi_k, dt)
-        omega_str = "{:.5f}".format(omega) + ' ' + str(v)
-        rospy.loginfo(str(omega))
-        pub.publish(omega)
+
+        x_k, y_k, fi_k = go_to_point(x_k, y_k, fi_k, x_ref, y_ref, dt)
+        #x_k, y_k, fi_k = go_to_point_opti(x_k, y_k, fi_k, x_ref, y_ref, fi_ref, v, dt)
+
+        hello_str = "x_k = {} y_k = {} fi_k = {} dt = {} ".format(x_k, y_k, fi_k, dt)
+
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
         rate.sleep()
- 
- 
+
 if __name__ == '__main__':
     try:
         talker()
     except rospy.ROSInterruptException:
         pass
-    
-    plt.plot(dx_arr,dy_arr)
-    #plt.show()
-    plt.savefig('der.png')
-    plt.clf()
-    
+
     plt.plot(x,y)
-    plt.savefig('pos.png')
-    plt.clf()
-    
-    plt.plot(fi_ref_arr)
-    plt.savefig('fi.png')
+    plt.show()
